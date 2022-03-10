2ec03cb9b94408f1f9760ac6cab6ca5acf420037
juaniMarderwald <jmarderwald87@gmail.com>
2022-03-10 11:30:06 -0300

    Primer commit Challenge Proyecto RaspCash

---
+++ src/main/java/Mindhub/RaspCash/RaspCashApplication.java
@@ -0,0 +1,13 @@
+package Mindhub.RaspCash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspCashApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaspCashApplication.class, args);
+	}
+
+}
