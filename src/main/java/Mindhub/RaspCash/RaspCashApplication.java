eda8392d354f7929d809e34a1546362bb93e3644
joel vallejos <92134801+gelneryus@users.noreply.github.com>
2022-03-11 09:50:56 -0300

    Update RaspCashApplication.java
    se me repite los datos no se porque ,solo agregue el @bean

--- src/main/java/Mindhub/RaspCash/RaspCashApplication.java
+++ src/main/java/Mindhub/RaspCash/RaspCashApplication.java
@@ -1,13 +1,32 @@
 package Mindhub.RaspCash;
 
+import Mindhub.RaspCash.respositories.*;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class RaspCashApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RaspCashApplication.class, args);
+	}
+@Bean
+	public CommandLineRunner initData(UsuarioRepositorio usuarioRepositorio, BilleteraRepositorio billeteraRepositorio,
+									  CarritoRepositorio carritoRepositorio, CriptoMonedaRepositorio criptoMonedaRepositorio,
+									  PrestamoRespositorio prestamoRespositorio, PrestamoUsuarioRespositorio prestamoUsuarioRespositorio,
+									  ProductoRepositorio productoRepositorio, TransaccionRepositorio transaccionRepositorio){
+
+	return (args) -> {
+
+
+
+
+
+
+
+		};
 	}
 
 }
