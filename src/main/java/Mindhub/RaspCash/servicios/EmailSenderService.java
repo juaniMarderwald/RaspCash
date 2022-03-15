305970fba639fad79a25e9b28619e8cb83c2f342
joel vallejos <92134801+gelneryus@users.noreply.github.com>
2022-03-15 11:08:48 -0300

    metodo de emails dinamicos correcion de error con la imagen

--- src/main/java/Mindhub/RaspCash/servicios/EmailSenderService.java
+++ src/main/java/Mindhub/RaspCash/servicios/EmailSenderService.java
@@ -2,7 +2,9 @@ package Mindhub.RaspCash.servicios;
 
 import org.springframework.stereotype.Service;
 
+import javax.mail.MessagingException;
+
 @Service
 public interface EmailSenderService {
-    public void sendSimpleEmailTo(String ReceptorEmail, String  MensajeAenviar, String tema);
+    public void sendSimpleEmailTo(String ReceptorEmail, String  MensajeAenviar, String tema,String archivoAdjunto) throws MessagingException;
 }
