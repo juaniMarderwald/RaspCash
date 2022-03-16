a26a383519956b8e7d3da66aa86913486773fb05
Juan Ignacio Marderwald <jmarderwald87@gmail.com>
2022-03-16 15:41:02 -0300

    campo de login y registro flotantes
    campo de login y registro flotantes

---
+++ src/main/resources/static/web/js/script2.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    $('#login').hide();
+
+    $('#open_close').click(function(){
+        $('#login').slideToggle();
+    })
+ })
+
+ $(document).ready(function(){
+    $('#registro').hide();
+
+    $('#open_close2').click(function(){
+        $('#registro').slideToggle();
+    })
+ })
